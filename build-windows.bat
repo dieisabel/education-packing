f0f8d458e6c75ad53a367e605bc2560120c8ce4f
Lavinchi <konstkadyr@gmail.com>
2021-01-05 21:33:52 +0200

    Удалил билды, добавил скрипты для компилирования

---
+++ build-windows.bat
@@ -0,0 +1,3 @@
+g++ .\src\main.cpp .\src\utils\figure_vars.cpp .\src\utils\math.cpp .\src\utils\point.cpp .\src\code\field.cpp .\src\code\figure.cpp .\src\code\packing.cpp .\src\code\packing_field.cpp .\src\code\status_bar.cpp -o .\dist\windows-build\packing.exe -lsfml-graphics -lsfml-window -lsfml-system
+.\dist\windows-build\packing.exe
+del .\dist\windows-build\packing.exe
